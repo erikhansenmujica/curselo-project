c38cac4250e77d042f8cd24c8226d0c56f8609f5
erikterminator <erikhansenmujica@gmail.com>
2019-07-04 16:41:18 -0300

    inicio real

---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+export default class main extends Component {
+
+    constructor(props) {
+        super(props);
+      }
+
+      render() {
+        return <div>LA RE CONCHA DE TU RE PUTA MADRE COMELA</div>;
+      }
+
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      filename: "bundle.js",
+      path: __dirname + '/public',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    context: __dirname,
+    module: {
+      rules: [
+        {
+          test: /jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+              presets: [
+              "@babel/preset-react",
+              "@babel/env"
+            ]
+        }
+        }
+      ]
+    },
+    devtool: 'source-map'
+}
+console.log(__dirname)
