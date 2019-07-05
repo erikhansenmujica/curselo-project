4f0e2ba487b88db02a83b3d8afbc3c2e11e2e551
mateocaraffo <mateocaraffo@gmail.com>
2019-07-05 15:43:08 -0300

    navbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,15 @@ import Home from "../components/Home";
 import { Route } from "react-router-dom";
 import CrearContainer from "../containers/CrearContainer";
 import InstructorCursos from "../containers/instructor/InstructorCursos";
+import NavBar from './NavBar'
 
 export default () => (
+  <div>
+    <NavBar />
   <div>
     <Route path="/crear" component={CrearContainer} />
     <Route path="/cursos" component={InstructorCursos} />
     <Route exact path="/" component={Home} />
+  </div>
   </div>
 );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default ({ handleChange, handleSubmit }) => (
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Curselo</a>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Log In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign In</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+)    
