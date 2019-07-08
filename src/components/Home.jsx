1cc7371d95fb592332186eec71937a8818e64caa
erikterminator <erikhansenmujica@gmail.com>
2019-07-08 16:14:15 -0300

    skere

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Vimeo from 'vimeo';
+let client = new Vimeo.Vimeo("{client_id}", "{client_secret}", "{access_token}");
 
+client.request({
+  method: 'GET',
+  path: '/tutorial'
+}, function (error, body, status_code, headers) {
+  if (error) {
+    console.log(error);
+  }
+
+  console.log(body);
+})
 const styles = {
   container:{
     display: 'flex',
