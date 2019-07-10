09ae1926e1c68b1c257b7776334406e8ab1994b9
grondo89 <grondo@gmail.com>
2019-07-10 18:40:30 -0300

    ahora rutea al nuevo curso cuando el instructor lo crea

--- src/components/instructor/SingleCursoInstructor.jsx
+++ src/components/instructor/SingleCursoInstructor.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default () => (
-    <div>
-        <h1>
-            HOLA HOLA HOLA! BIENVENIDOS A SINGLE CURSO!
-        </h1>
+  <div>
+    <h1>HOLA HOLA HOLA! BIENVENIDOS A SINGLE CURSO!</h1>
+    {/* <form action="" onSubmit={handleUpload}>
 
+<label htmlFor='icon'>PDF File</label>
+<input type='file' name='pdf' accept='.pdf' onChange={handleSetFile}/>
+<button type='submit'>Upload PDF!</button>
 
-    </div>
-)
+</form> */}
+  </div>
+);
--- src/containers/instructor/CrearContainer.jsx
+++ src/containers/instructor/CrearContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CrearCurso from "../../components/instructor/CrearCurso";
-import { firebase, db } from "../../config/app";
+import { db } from "../../config/app";
 
 export default class CrearContainer extends React.Component {
   constructor(props) {
@@ -11,14 +11,10 @@ export default class CrearContainer extends React.Component {
       precio: 0,
       descripcion: "",
       contenido: "",
-      anexos: "",
-      file: "ffffffff",
-      propDePrueba: ""
+      anexos: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
-    this.handleSetFile = this.handleSetFile.bind(this);
   }
   handleChange(e) {
     this.setState({
@@ -27,29 +23,18 @@ export default class CrearContainer extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    db.collection("cursos").add({
-      titulo: this.state.titulo,
-      duracion: this.state.duracion,
-      precio: this.state.precio,
-      descripcion: this.state.descripcion,
-      contenido: this.state.contenido,
-      anexos: this.state.anexos
-    });
-  }
-
-  handleSetFile(e) {
-    e.preventDefault();
-    let f = e.target.files[0];
-    this.setState({
-      file: f
-    });
-  }
-
-  handleUpload(e) {
-    e.preventDefault();
-    const file = this.state.file
-    const storageRef = firebase.storage().ref(`/files/${file.name}`);
-    storageRef.put(file).then(file => console.log("FILE?", file));
+    db.collection("cursos")
+      .add({
+        titulo: this.state.titulo,
+        duracion: this.state.duracion,
+        precio: this.state.precio,
+        descripcion: this.state.descripcion,
+        contenido: this.state.contenido,
+        anexos: this.state.anexos
+      })
+      .then(data => {
+        this.props.history.push(`cursos/instructor/${data.id}`);
+      });
   }
 
   render() {
@@ -57,8 +42,6 @@ export default class CrearContainer extends React.Component {
       <CrearCurso
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
-        handleUpload={this.handleUpload}
-        handleSetFile={this.handleSetFile}
       />
     );
   }
