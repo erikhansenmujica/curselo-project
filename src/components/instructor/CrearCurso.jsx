6f915dd03f0c4c0da02bda6ae68d52fc43927349
Rosanavpu <rosanap06@gmail.com>
2019-07-10 19:25:32 -0300

    Diseno de Modal Crear Curso

--- src/components/instructor/CrearCurso.jsx
+++ src/components/instructor/CrearCurso.jsx
@@ -1,107 +1,143 @@
 import React from "react";
 
 const styles = {
-  button: {
-    marginLeft: "1%",
+  alineacion: {
+    position: "center"
   },
-  inputDescription: {
-    height: "200px"
-  },
-  title: {
-    textAlign: "center"
+  inputText: {
+    height: "150px"
+  }, 
+  buttons:{
+    display:"block", 
+    margin:"auto"
   }
 };
 
-export default ({ handleChange, handleSubmit, handleUpload, handleSetFile }) => (
-  <div className="container">
-    <h2 style={styles.title}>Carga tus conocimientos </h2>
-    <br />
-    <br />
-    <form
-      onSubmit={e => {
-        handleSubmit(e);
-        document
-          .querySelectorAll(".form-control")
-          .forEach(input => (input.value = ""));
-      }}
+export default ({ handleChange, handleSubmit }) => (
+  <div>
+    <button
+      type="button"
+      className="btn btn-primary"
+      data-toggle="modal"
+      data-target="#exampleModal"
+      style={styles.buttons}
     >
-      <div className="form-group">
-        <label>Título</label>
-        <input
-          type="text"
-          name="titulo"
-          className="form-control"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Descripción</label>
-        <textarea
-          style={styles.inputDescription}
-          name="descripcion"
-          type="text"
-          className="form-control"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Contenido</label>
-        <textarea
-          style={styles.inputDescription}
-          name="contenido"
-          type="text"
-          className="form-control"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Precio</label>
-        <input
-          name="precio"
-          type="number"
-          className="form-control"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Anexos</label>
-        <input
-          type="text"
-          name="anexos"
-          className="form-control"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label>Duración</label>
-        <input
-          name="duracion"
-          type="text"
-          className="form-control"
-          onChange={handleChange}
-        />
+      Crea tu Curso
+    </button>
 
+    <div
+      className="modal fade bd-example-modal-lg"
+      id="exampleModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="myLargeModalLabel"
+      aria-hidden="true"
+      style={styles.tamano}
+    >
+      <div className="modal-dialog modal-lg" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4
+              className="modal-title"
+              id="exampleModalLabel"
+              style={styles.alineacion}
+            >
+              Crea Tu Curso
+            </h4>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <form
+              onSubmit={e => {
+                handleSubmit(e);
+                document
+                  .querySelectorAll(".form-control")
+                  .forEach(input => (input.value = ""));
+              }}
+            >
+              <div className="form-group">
+                <label className="col-form-label">Titulo:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="titulo"
+                  onChange={handleChange}
+                  id="recipient-name"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label">Precio:</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  name="precio"
+                  onChange={handleChange}
+                  id="recipient-name"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label">Duracion:</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  name="duracion"
+                  onChange={handleChange}
+                  id="recipient-name"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label">Anexos:</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  name="anexos"
+                  onChange={handleChange}
+                  id="recipient-name"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label">Descripcion:</label>
+                <textarea
+                  className="form-control"
+                  name="descripcion"
+                  onChange={handleChange}
+                  id="message-text"
+                  style={styles.inputText}
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label">Contenido:</label>
+                <textarea
+                  className="form-control"
+                  name="contenido"
+                  onChange={handleChange}
+                  id="message-text"
+                  style={styles.inputText}
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Cancelar
+                </button>
+                <button type="submit" className="btn btn-primary">
+                  Crear
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-
-
-
-      <button type="submit" className="btn btn-primary">
-        Crear Curso
-      </button>
-      <button type="submit" className="btn btn-primary">
-        Cancelar
-      </button>
-    </form>
-      <form action="" onSubmit={handleUpload}>
-
-        <label htmlFor='icon'>PDF File</label>
-        <input type='file' name='pdf' accept='.pdf' onChange={handleSetFile}/>
-        <button type='submit'>Upload PDF!</button>
-
-      </form>
-    <br />
-    <br />
-    <br />
-    <br />
+    </div>
   </div>
 );
