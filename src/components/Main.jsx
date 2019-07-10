11c8c9dda66350eb55517d73639bea416b53881c
grondo89 <grondo@gmail.com>
2019-07-10 18:13:40 -0300

    single Curso alumno e instructor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import HomeContainer from "../containers/HomeContainer";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import CrearContainer from "../containers/instructor/CrearContainer";
 import InstructorCursos from "../containers/instructor/InstructorCursos";
-import InstructorContainer from "../containers/instructor/InstructorContainer"
+import InstructorContainer from "../containers/instructor/InstructorContainer";
 import NavBar from "./NavBar";
+import SingleCursoInstructorContainer from "../containers/instructor/SingleCursoInstructorContainer";
+import SingleCursoAlumnoContainer from "../containers/SingleCursoAlumnoContainer"
 
 export default () => (
   <div>
     <NavBar />
-    <Route path="/crear" component={CrearContainer} />
-    <Route path="/cursos" component={InstructorCursos} />
-    <Route path= "/instructor" component={InstructorContainer} />
-    <Route exact path="/" component={HomeContainer} />
+    <Switch>
+      <Route path="/crear" component={CrearContainer} />
+      <Route
+        exact
+        path="/cursos/instructor/:cursoId"
+        component={SingleCursoInstructorContainer}
+      />
+      <Route
+        exact
+        path="/cursos/alumno/:cursoId"
+        component={SingleCursoAlumnoContainer}
+      />
+      <Route path="/cursos" component={InstructorCursos} />
+      <Route path="/instructor" component={InstructorContainer} />
+      <Route exact path="/" component={HomeContainer} />
+    </Switch>
   </div>
 );
