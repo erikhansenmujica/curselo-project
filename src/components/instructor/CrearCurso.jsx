dfdf90b98505ff0097a5ce57978e88e27b8db248
Rosanavpu <rosanap06@gmail.com>
2019-07-11 14:08:21 -0300

    Arreglo del homesish

--- src/components/instructor/CrearCurso.jsx
+++ src/components/instructor/CrearCurso.jsx
@@ -1,143 +1,128 @@
 import React from "react";
 
 const styles = {
-  alineacion: {
-    position: "center"
+  button: {
+    display: "flex",
+    justifyContent: "space-around"
   },
-  inputText: {
-    height: "150px"
-  }, 
-  buttons:{
-    display:"block", 
-    margin:"auto"
+  inputDescription: {
+    height: "200px"
+  },
+  title: {
+    textAlign: "center"
+  },
+  cards: {
+    borderStyle: "solid",
+    borderColor: "#7CE2EF"
+  },
+  colorTitle: {
+    backgroundColor: "#8BE3EF"
+  },
+  position: {
+    marginLeft: "25%",
+    marginTop:"2%",
+    width:"70%"
   }
 };
 
-export default ({ handleChange, handleSubmit }) => (
-  <div>
-    <button
-      type="button"
-      className="btn btn-primary"
-      data-toggle="modal"
-      data-target="#exampleModal"
-      style={styles.buttons}
+export default ({
+  handleChange,
+  handleSubmit,
+  handleUpload,
+  handleSetFile
+}) => (
+  <div className="container" style={styles.position}>
+  
+    <form
+      onSubmit={e => {
+        handleSubmit(e);
+        document
+          .querySelectorAll(".form-control")
+          .forEach(input => (input.value = ""));
+      }}
     >
-      Crea tu Curso
-    </button>
-
-    <div
-      className="modal fade bd-example-modal-lg"
-      id="exampleModal"
-      tabIndex="-1"
-      role="dialog"
-      aria-labelledby="myLargeModalLabel"
-      aria-hidden="true"
-      style={styles.tamano}
-    >
-      <div className="modal-dialog modal-lg" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4
-              className="modal-title"
-              id="exampleModalLabel"
-              style={styles.alineacion}
-            >
-              Crea Tu Curso
-            </h4>
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <form
-              onSubmit={e => {
-                handleSubmit(e);
-                document
-                  .querySelectorAll(".form-control")
-                  .forEach(input => (input.value = ""));
-              }}
-            >
-              <div className="form-group">
-                <label className="col-form-label">Titulo:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="titulo"
-                  onChange={handleChange}
-                  id="recipient-name"
-                />
-              </div>
-              <div className="form-group">
-                <label className="col-form-label">Precio:</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  name="precio"
-                  onChange={handleChange}
-                  id="recipient-name"
-                />
-              </div>
-              <div className="form-group">
-                <label className="col-form-label">Duracion:</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  name="duracion"
-                  onChange={handleChange}
-                  id="recipient-name"
-                />
-              </div>
-              <div className="form-group">
-                <label className="col-form-label">Anexos:</label>
-                <input
-                  type="number"
-                  className="form-control"
-                  name="anexos"
-                  onChange={handleChange}
-                  id="recipient-name"
-                />
-              </div>
-              <div className="form-group">
-                <label className="col-form-label">Descripcion:</label>
-                <textarea
-                  className="form-control"
-                  name="descripcion"
-                  onChange={handleChange}
-                  id="message-text"
-                  style={styles.inputText}
-                />
-              </div>
-              <div className="form-group">
-                <label className="col-form-label">Contenido:</label>
-                <textarea
-                  className="form-control"
-                  name="contenido"
-                  onChange={handleChange}
-                  id="message-text"
-                  style={styles.inputText}
-                />
-              </div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Cancelar
-                </button>
-                <button type="submit" className="btn btn-primary">
-                  Crear
-                </button>
-              </div>
-            </form>
-          </div>
+      <div className="card bg mb-3" style={styles.cards}>
+        <div className="card-header">Titulo</div>
+        <div className="card-body">
+          <input
+            type="text"
+            name="titulo"
+            className="form-control"
+            onChange={handleChange}
+          />
         </div>
       </div>
-    </div>
+      <div className="card bg mb-3" style={styles.cards}>
+        <div className="card-header">Precio</div>
+        <div className="card-body">
+          <input
+            name="precio"
+            type="number"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="card bg mb-3" style={styles.cards}>
+        <div className="card-header">Anexos</div>
+        <div className="card-body">
+          <input
+            type="text"
+            name="anexos"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="card bg mb-3" style={styles.cards}>
+        <div className="card-header">Duracion</div>
+        <div className="card-body">
+          <input
+            name="duracion"
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="card bg-light mb-3" style={styles.cards}>
+        <div className="card-header">Descripcion</div>
+        <div className="card-body">
+          <textarea
+            style={styles.inputDescription}
+            name="descripcion"
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="card bg-light mb-3" style={styles.cards}>
+        <div className="card-header">Contenido</div>
+        <div className="card-body">
+          <textarea
+            style={styles.inputDescription}
+            name="contenido"
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div style={styles.button}>
+        <button type="submit" className="btn btn-primary">
+          Cancelar
+        </button>
+        <button type="submit" className="btn btn-primary">
+          Crear Curso
+        </button>
+      </div>
+      <br />
+      <br />
+    </form>
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 );
