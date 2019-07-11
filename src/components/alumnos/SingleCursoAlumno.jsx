cb48eb312f43ddacc7c9433c1d3dba081f0daeaf
erikterminator <erikhansenmujica@gmail.com>
2019-07-11 17:03:57 -0300

    skere

--- src/components/alumnos/SingleCursoAlumno.jsx
+++ src/components/alumnos/SingleCursoAlumno.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default ({curso}) => (
-    <div>
-        {curso&&<h1>{curso.titulo}</h1>}
-            
+    <div style={{
+        height:"100vh",
+        width:"100vw",
+        backgroundColor:"#F3F6FC"}}>
+        <div style={{
+            borderRadius:"4px",
+            backgroundColor:"white",
+            height: "60vh",
+            width: "80vw",
+            marginLeft:"10%",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            borderColor:"#E4E9F4",
+            display:"flex",
+            flexDirection: "row",
+        }}>
+                <div style={{
+                    float:"left"
+                }}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSFiS3k5Xqg9CFuoghOVJPtpO8Wgz4QpaVbx0Ei5dOJoBWKoMY" alt="" height="300vh"/>
+                </div>
+            <div style={{
+                marginTop:"5%",
+                float:"left"
+            }}>
+                {curso&&<h3>{curso.titulo}</h3>}
+                {curso&&<p>{curso.descripcion}</p>}
+            </div>
+        </div>
+
 
     </div>
 )
