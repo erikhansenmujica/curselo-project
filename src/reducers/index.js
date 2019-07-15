1845f682c6dcbcddabb2b7f7ff410358fb6a33ad
erikterminator <erikhansenmujica@gmail.com>
2019-07-15 16:13:48 -0300

    refactor

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
 import {  firebaseReducer } from 'react-redux-firebase'
-
+import cursosAlumnoReducer from "../reducers/cursosAlumnoReducer"
+import cursosInstructorReducer from "../reducers/cursosInstructorReducer"
 
 // Add firebase to reducers
 export const rootReducer = combineReducers({
+  alumnoCursos: cursosAlumnoReducer,
+  instructorCursos: cursosInstructorReducer,
   firestore: firestoreReducer,
   firebase: firebaseReducer
 })
