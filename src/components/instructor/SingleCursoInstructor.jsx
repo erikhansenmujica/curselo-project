a190e2558b676e2482fc6f07aebeb7c5d335bfe8
erikterminator <erikhansenmujica@gmail.com>
2019-07-16 16:30:58 -0300

    crear secciones

--- src/components/instructor/SingleCursoInstructor.jsx
+++ src/components/instructor/SingleCursoInstructor.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import SectionTopicContainer from "../../containers/instructor/Section&TopicContainer" 
+import SectionTopicContainer from "../../containers/instructor/Section&TopicContainer";
 
 const styles = {
   nav: {
-    width:"60vw"
+    width: "60vw"
   },
   button: {
     display: "inline",
@@ -13,49 +13,73 @@ const styles = {
 };
 
 export default props => (
-  <div style={{  
-    marginLeft:"15%",
-    marginTop:"-2%"}}>
+  <div
+    style={{
+      marginLeft: "15%",
+      marginTop: "-2%"
+    }}
+  >
     {props.curso.name && (
-      <div >
+      <div>
         <div className="media">
           <div className="media-body">
             <h5 className="mt-0">Curso: {props.curso.name}</h5>
           </div>
         </div>
-        <SectionTopicContainer cursoId={props.curso.id}/>
+        <div>
+          <a
+            href=""
+            className="btn btn-default btn-rounded mb-4"
+            data-toggle="modal"
+            data-target="#modalContactForm"
+          >
+            Add Section
+          </a>
+        </div>
+        <SectionTopicContainer cursoId={props.curso.id} />
       </div>
     )}
-      <div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Write to us</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-
-        <div class="md-form mb-5">
-          <i class="fas fa-tag prefix grey-text"></i>
-          <input type="text" id="form32" class="form-control validate"/>
-          <label data-error="wrong" data-success="right" for="form32">Subject</label>
+    <div
+      className="modal fade"
+      id="modalContactForm"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="myModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h4 className="modal-title w-100 font-weight-bold">Add Section</h4>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form onSubmit={props.handleSubmit}>
+          <div className="modal-body mx-3">
+            <div className="md-form mb-5">
+              <i className="fas fa-tag prefix grey-text" />
+              <input type="text" id="form32" className="form-control validate" onChange={props.handleChange} />
+            </div>
+          </div>
+          <div className="modal-footer d-flex justify-content-center">
+            <button
+            type="submit"
+              className="btn btn-unique"
+              data-toggle="modal"
+              data-target="#modalContactForm"
+            >
+              Send <i className="fas fa-paper-plane-o ml-1" />
+            </button>
+          </div>
+            </form>
         </div>
-
-      
-      </div>
-      <div class="modal-footer d-flex justify-content-center">
-        <button class="btn btn-unique">Send <i class="fas fa-paper-plane-o ml-1"></i></button>
       </div>
     </div>
-  </div>
-</div>
-
-<div class="text-center">
-  <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalContactForm">Launch
-    Modal Contact Form</a>
-</div>
   </div>
 );
