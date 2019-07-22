090c7c7802b9aa62191eade4004268d904b9c604
erikterminator <erikhansenmujica@gmail.com>
2019-07-22 18:34:57 -0300

    borrar curso, borrar seccion

--- src/containers/instructor/InstructorCursosContainer.jsx
+++ src/containers/instructor/InstructorCursosContainer.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import InstructorCursos from "../../components/instructor/InstructorCursos";
 import { connect } from "react-redux";
-import { fetchCursosInstructor } from "../../action-creators/cursosInstructor";
+import { fetchCursosInstructor, deleteCourse } from "../../action-creators/cursosInstructor";
 
 import { auth } from "../../config/app";
 
 class InstructorCursosContainer extends Component {
   constructor(props) {
     super(props);
+    this.deleteButton=this.deleteButton.bind(this)
   }
 
   componentDidMount() {
     this.props.getCursos(auth.currentUser.uid);
   }
 
+  deleteButton(id){
+    deleteCourse(id).then(()=> this.props.getCursos(auth.currentUser.uid))
+  }
+
   render() {
     
-    return <InstructorCursos cursos={this.props.cursos} />;
+    return <InstructorCursos cursos={this.props.cursos} deleteButton={this.deleteButton}/>;
   }
 }
 
