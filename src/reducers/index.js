3c399395fb798bdd915cfcb0d57d5e2fc7533697
Rosanavpu <49411042+Rosanavpu@users.noreply.github.com>
2019-07-25 15:09:59 -0300

    Update index.js
    Cambios que trajo el pull de develop

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,9 @@ import { firebaseReducer } from "react-redux-firebase";
 import cursosAlumnoReducer from "../reducers/cursosAlumnoReducer";
 import cursosInstructorReducer from "../reducers/cursosInstructorReducer";
 import createUserReducer from "../reducers/createUserReducer";
-<<<<<<< HEAD
 import searchReducer from "../reducers/searchReducer"
-//import updateCourseReducer from "../reducers/updateCourseReducer"
-=======
 import searchReducer from "../reducers/searchReducer";
 import purchaseReducer from "../reducers/purchaseReducer"
->>>>>>> 1e8dfbf79ca990dbfc115c20fca630c44fd7a5f1
 
 // Add firebase to reducers
 export const rootReducer = combineReducers({
